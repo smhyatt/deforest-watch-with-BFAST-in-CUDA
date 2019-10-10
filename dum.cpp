533343df7c7b2df1ec14ae1b2db1e484f31802de
S.Hyatt <smhyatt@protonmail.com>
2019-10-10 16:55:56 +0200

    New additions.

---
+++ dum.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[]){
+	
+	printf("Hello Rune\n");
+	return 0;
+}
+
--- seq.cpp
+++ seq.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
 
 
 
@@ -9,21 +11,32 @@
 int main(int argc, char const *argv[]) {
 	
 	if (argc != 10) {
-		printf("Missing input.\n");
+		std::cerr << "Not enough arguments!\n";
+      	return -1;
 	}
 
-	int trend = argv[1];
-	int k = argv[2];
-	int m = argv[3];
-	int n = argv[4];
-	int N = argv[5];
-	float freq = argv[6];
-	float hfrac = argv[7];
-	float lam = argv[8];
-	int* mappingindices = malloc(N*sizeof(int));
-	mappingindices = argv[9];
-	float ** sample = malloc((N*m)*sizeof(float));
-	sample = argv[10];
+	int trend 			= atoi(argv[1]);
+	int k 				= atoi(argv[2]);
+	int m 				= atoi(argv[3]);
+	int n 				= atoi(argv[4]);
+	int N 				= atoi(argv[5]);
+	float freq 			= atoi(argv[6]);
+	float hfrac 		= atoi(argv[7]);
+	float lam 			= atoi(argv[8]);
+	int size 			= N*sizeof(int);
+	int* mappingindices = (int*)malloc(size);
+	for (int i = 0; i < N; i++) {
+		mappingindices[i] = argv[9][i];
+	}
+
+	int sampsize 		= (N*m)*sizeof(float);
+	float ** sample 	= (float**)malloc(sampsize);
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < N; j++) {
+			sample[i][j] = argv[10][i+j+sizeof(float)];
+		}
+	}
+
 
 	printf("%A\n", trend);
 	printf("%A\n", k);
