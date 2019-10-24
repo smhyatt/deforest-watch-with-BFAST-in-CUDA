a8b84442737733517bd1568b35b232b8e7c580c2
S.Hyatt <smhyatt@protonmail.com>
2019-10-24 17:36:02 +0200

    Additions.

--- kernel-dev/sequential.cu.h
+++ kernel-dev/sequential.cu.h
@@ -144,22 +144,16 @@ void mkXsqrOptim(uint n, uint N, uint m, float* X, float* XT, float* sample, flo
     // }
     // printf("\n\n\n");
 
-    float* YT = (float*) calloc(N*m,sizeof(float));
-    transpose(sample, YT, m, N);
     // printf("************** EFTER TRANSPOSE ****************\n");
     // for (int i = 0; i < N*m; i++) {
     //     printf("%f, ", YT[i]);
     // }
-    printf("\n\n\n");
-    int R = 30;
-    int T1 = K;
-    int T2 = K; 
-
+    const int R = 30;
     for (int ii = 0; ii < m; ii+=R) {                                  // forall, grid.z
-        for (int jj1 = 0; jj1 < K; jj1+=T1) {                          // forall, grid.y
-            for (int jj2 = 0; jj2 < K; jj2+=T2){                       // forall, grid.x
-                for (int j1 = jj1; j1 < min(jj1+T1, K); j1++) {        // forall, block.y
-                    for (int j2 = jj2; j2 < min(jj2+T2, K); j2++) {    // forall, block.x
+        //for (int jj1 = 0; jj1 < K; jj1+=T1) {                          // forall, grid.y
+        //    for (int jj2 = 0; jj2 < K; jj2+=T2){                       // forall, grid.x
+                for (int j1 = 0; j1 < K; j1++) {        // forall, block.y
+                    for (int j2 = 0; j2 < K; j2++) {    // forall, block.x
 
                         // float yqsh[R];          // size R, shared memory
                         float acc[R]; //  = calloc(R,sizeof(float));          // size R, registers
@@ -187,18 +181,16 @@ void mkXsqrOptim(uint n, uint N, uint m, float* X, float* XT, float* sample, flo
                                 }
                             }
 
-                            for (int i2 = 0; i2 < R; i2++) { // fully unroll
-                                if (ii+i2 < m) {
-                                    // Xsqr[pix*K*K + i*K + j] = acc;
-                                    Xsqr[(ii+i2)*(K*K) + j1*K + j2*K] = acc[i2];
-                                }
+                        }
+                        for (int i2 = 0; i2 < R; i2++) { // fully unroll
+                            if (ii+i2 < m) {
+                                // Xsqr[pix*K*K + i*K + j] = acc;
+                                Xsqr[(ii+i2)*(K*K) + j1*K + j2*K] = acc[i2];
                             }
                         }
                     }
                 }
             }
-        }
-    }
 
 }
 
