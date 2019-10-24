ad8f33093c7530d6ccfc9206457db2dfc6fe4eab
borune <runetcp@gmail.com>
2019-10-24 17:33:09 +0200

    asdf

--- kernel-dev/sequential-gj.cu.h
+++ kernel-dev/sequential-gj.cu.h
@@ -24,7 +24,7 @@ void gaussJordanG(uint M, uint K, float* A, float* AI){
         for (uint k1 = 0; k1 < K; k1++){
             for (uint k2 = 0; k2 < 2*K; k2++){
                 if (k2<K) {
-                    Ash[k1*2*K + k2] = A[i*K*2*K + k1*2*K + k2];
+                    Ash[k1*2*K + k2] = A[i*K*K + k1*K + k2];
                 } else {
                     Ash[k1*2*K + k2] = (float) (k2 == (K+k1));
                 }
@@ -32,46 +32,57 @@ void gaussJordanG(uint M, uint K, float* A, float* AI){
             }
         }
 
-        // // Gauss-Jordan Elimination:
-        // for (uint q = 0; q < K; q++){               // sequential
-        //     float vq = Ash[q];
-        //     for (uint k1 = 0; k1 < K; k1++){        // parallel block.y
-        //         for (uint k2 = 0; k2 < 2*K; k2++){  // parallel block.x
-        //             float tmp = 0.0;
-        //             if (vq == 0.0) {
-        //                 tmp = Ash[k1*2*K + k2];
-        //             } else {
-        //                 float x = Ash[k2] / vq;
-        //                 if (k1 == (K-1)){
-        //                     tmp = x;
-        //                 } else {
-        //                     tmp = Ash[(k1+1)*2*K + k2] - Ash[(k1+1)*2*K + q] *x;
-        //                 }
-        //             }
-        //             // barrier
-        //             Ash[k1*2*K + k2] = tmp;
-        //             // barrier
+        // for (uint k1 = 0; k1 < K; k1++){
+        //     for (uint k2 = 0; k2 < 2*K; k2++){
+        //         if (k2<K) {
+        //             Ash[k1*2*K + k2] = A[i*K*2*K + k1*2*K + k2];
+        //         } else {
+        //             Ash[k1*2*K + k2] = (float) (k2 == (K+k1));
         //         }
+        //         // barrier
         //     }
         // }
 
-        // after gauss jordan copies id matrix to AI
-        for (int k1 = 0; k1 < K; k1++) {
-            for (int k2 = 0; k2 < K; k2++) {
-                uint XinvIdx  = k1*(K*2) + k2;
-                uint XlessIdx = i*K*K + k1*K + k2;
-                AI[XlessIdx] = Ash[XinvIdx];
+        // Gauss-Jordan Elimination:
+        for (uint q = 0; q < K; q++){               // sequential
+            float vq = Ash[q];
+            for (uint k1 = 0; k1 < K; k1++){        // parallel block.y
+                for (uint k2 = 0; k2 < 2*K; k2++){  // parallel block.x
+                    float tmp = 0.0;
+                    if (vq == 0.0) {
+                        tmp = Ash[k1*2*K + k2];
+                    } else {
+                        float x = Ash[k2] / vq;
+                        if (k1 == (K-1)){
+                            tmp = x;
+                        } else {
+                            tmp = Ash[(k1+1)*2*K + k2] - Ash[(k1+1)*2*K + q] *x;
+                        }
+                    }
+                    // barrier
+                    Ash[k1*2*K + k2] = tmp;
+                    // barrier
+                }
             }
         }
 
-        // // collective copy shared-to-global mem:
+        // // after gauss jordan copies id matrix to AI
         // for (int k1 = 0; k1 < K; k1++) {
         //     for (int k2 = 0; k2 < K; k2++) {
-        //         uint XinvIdx  = k1*(K*2) + k2+K;
+        //         uint XinvIdx  = k1*(K*2) + k2;
         //         uint XlessIdx = i*K*K + k1*K + k2;
         //         AI[XlessIdx] = Ash[XinvIdx];
         //     }
         // }
+
+        // collective copy shared-to-global mem:
+        for (int k1 = 0; k1 < K; k1++) {
+            for (int k2 = 0; k2 < K; k2++) {
+                uint XinvIdx  = k1*(K*2) + k2+K;
+                uint XlessIdx = i*K*K + k1*K + k2;
+                AI[XlessIdx] = Ash[XinvIdx];
+            }
+        }
         free(Ash);
     }
 }
