3f56bb5b48b686fe5f75effb317fefa0ecdedbc6
borune <runetcp@gmail.com>
2019-10-27 18:16:30 +0100

    hmmm ahha

--- kernel-dev/kernels-optim.cu.h
+++ kernel-dev/kernels-optim.cu.h
@@ -146,11 +146,9 @@ __global__ void ker3(uint M, uint K, float* A, float* AI){
     // float* Ash = &shared[0];
     // float* AshTmp = &shared[2*K*K];
 
-    // shared[k1*K + k2]
-    float val = A[i*K*K + k1*K + k2] - 1;
+    shared[k1*K + k2] = A[i*K*K + k1*K + k2] - 1;
 
-    // AI[i*K*K + k1*K + k2] = shared[k1*K + k2];
-    AI[i*K*K + k1*K + k2] = val;
+    AI[i*K*K + k1*K + k2] = shared[k1*K + k2];
 
     // extern __shared__ float Ash[];
     // float* Ash    = (float*) calloc(2*K*K,sizeof(float));
