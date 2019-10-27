7d04ca39f0766b005752d185f8cfdfc623189aff
borune <runetcp@gmail.com>
2019-10-27 21:58:36 +0100

    ok

--- kernel-dev/kernels-optim.cu.h
+++ kernel-dev/kernels-optim.cu.h
@@ -163,6 +163,7 @@ __global__ void ker3(uint M, uint K, float* A, float* AI){
         Ash[k1*2*K + k2] = (float) (k1+K == k2);
     }
 
+#if 0
     #pragma unroll
     for (uint q = 0; q < 2*K; q++){               // sequential
         float vq = Ash[q];
@@ -191,6 +192,8 @@ __global__ void ker3(uint M, uint K, float* A, float* AI){
         AshTmp = Ash;
         Ash    = tmp2;
     }
+
+#endif
 
     if (K <= k2) {
         AI[i*K*K + k1*K + k2 - K] = Ash[k1*2*K + k2];
