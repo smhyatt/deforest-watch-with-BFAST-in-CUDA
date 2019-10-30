3e8a08c9a1e279bd0b85d4faba5b4ae82f4afe8b
borune <runetcp@gmail.com>
2019-10-30 16:58:00 +0100

    new pi

--- kernel-dev2/sequential.cu.h
+++ kernel-dev2/sequential.cu.h
@@ -6,7 +6,8 @@
 
 #include "helper.cu.h"
 
-#define PI 3.14159265
+// #define PI 3.14159265
+#define PI 3.141592653589793115997963468544185161590576171875
 #define F32_MIN -FLT_MAX
 #define I32_MIN -2147483648
 typedef unsigned int uint;
