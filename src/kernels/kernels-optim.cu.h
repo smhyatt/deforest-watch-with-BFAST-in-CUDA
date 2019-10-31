ee24de10a5ae74150082a536a2af83cba00c1531
borune <runetcp@gmail.com>
2019-11-01 00:01:02 +0100

    ok

--- src/kernels/kernels-optim.cu.h
+++ src/kernels/kernels-optim.cu.h
@@ -680,7 +680,7 @@ __global__ void ker10(float lam, uint m, uint n, uint N, float* bound,
     uint ns = nss[pix];
     uint h = hs[pix];
     float sigma = sigmas[pix];
-    val_inds = val_indss[pix];
+    int val_inds = val_indss[pix];
 
     float tmp;
     if(i >= Ns-ns){
